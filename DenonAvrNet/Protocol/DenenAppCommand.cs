b5547104911fdb01099a567499a28f85c186ed2e
Maxi <ricokowalski.02@gmail.com>
2026-09-14 11:26:16 +0200

    Fixed some old request structures

---
+++ DenonAvrNet/Protocol/DenenAppCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace DenonAvrNet.Protocol;
+
+/// <summary>Creates the XML requests understood by Denon's AppCommand API.</summary>
+internal static class DenonAppCommand
+{
+    internal static string CreateMainZoneStatusRequest()
+    {
+        var commands = new[]
+        {
+            "GetAllZonePowerStatus",
+            "GetAllZoneVolume",
+            "GetAllZoneMuteStatus",
+            "GetAllZoneSource",
+            "GetDeletedSource"
+        };
+
+        return CreateTransaction(commands).ToString(SaveOptions.DisableFormatting);
+    }
+
+    private static XElement CreateTransaction(IEnumerable<string> commands) =>
+        new("tx", commands.Select(command => new XElement("cmd", new XAttribute("id", "1"), command)));
+}
